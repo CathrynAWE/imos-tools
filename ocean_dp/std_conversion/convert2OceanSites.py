a59c9d1b2a697631c12f868e88508f3dc7db2d9b
CathrynAWE <cathryn.wynnedwards@utas.edu.au>
2020-07-29 10:18:56 +1000

    additional attributes

--- ocean_dp/std_conversion/convert2OceanSites.py
+++ ocean_dp/std_conversion/convert2OceanSites.py
@@ -6,7 +6,7 @@ import argparse
 import re
 import numpy
 
-# IMOS file format convertion to OceanSITES format
+# IMOS file format conversion to OceanSITES format
 # Pete Jansen 2018-10-09
 
 from dateutil.parser import parse
@@ -80,14 +80,14 @@ fileProductType = fileProductTypeSplit[0]
 
 fileTimeFormat = "%Y%m%d"
 ncTimeFormat = "%Y-%m-%dT%H:%M:%SZ"
-sensor = fileProductTypeSplit[2]
+sensor = fileProductTypeSplit[3]
 
 # Generate OceanSITES file name
 outputName = "OS" \
              + "_" + "SOTS" \
              + "_" + fileProductType + "-" + fileProductTypeSplit[1] \
              + "_D" \
-             + "_" + sensor + "-" + fileProductTypeSplit[3] \
+             + "_" + sensor + "-" + splitParts[-1] \
              + ".nc"
 
 print("output file : %s" % outputName)
@@ -104,18 +104,16 @@ globalAttributeBlackList = ['time_coverage_end', 'time_coverage_start',
                             'compliance_checks_passed', 'compliance_checker_version', 'compliance_checker_imos_version',
                             'date_created',
                             'deployment_code',
-                            'geospatial_lat_max',
-                            'geospatial_lat_min',
-                            'geospatial_lon_max',
-                            'geospatial_lon_min',
-                            'geospatial_vertical_max',
-                            'geospatial_vertical_min',
                             'instrument',
                             'instrument_nominal_depth',
                             'instrument_sample_interval',
                             'instrument_serial_number',
                             'quality_control_log',
-                            'history', 'netcdf_version']
+                            'history', 'acknowledgement', 'abstract', 'author', 'author_email', 'file_version']
+
+# these attributes are actually part of the OceanSites list, so I took them off the BlackList
+#, 'netcdf_version','geospatial_lat_max', 'geospatial_lat_min', 'geospatial_lon_max', 'geospatial_lon_min',
+# 'geospatial_vertical_max', 'geospatial_vertical_min',]
 
 # global attributes
 
@@ -138,7 +136,10 @@ ncOut.createDimension('LATITUDE', 1)
 ncOut.createDimension('LONGITUDE', 1)
 
 history = dsIn.getncattr("history")
+instrumentName = dsIn.getncattr("instrument")
 serialNumber = dsIn.getncattr("instrument_serial_number")
+deployment_start = dsIn.getncattr("time_deployment_start")
+deployment_end = dsIn.getncattr("time_deployment_end")
 
 dsIn.close()
 
@@ -146,29 +147,38 @@ ncOut.setncattr("time_coverage_start", dates[0].strftime(ncTimeFormat))
 ncOut.setncattr("time_coverage_end", dates[-1].strftime(ncTimeFormat))
 ncOut.setncattr("date_created", datetime.utcnow().strftime(ncTimeFormat))
 ncOut.setncattr("history", history + '\n' + datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC : Convert from IMOS file : ") + fileName)
-
+ncOut.setncattr("acknowledgement", "We acknowledge support from the following agencies: the Australian Antarctic Program Partnership (AAPP), the Antarctic Climate and Ecosystems Cooperative Research Centre (ACE CRC), the Integrated Marine Observing System (www.imos.org.au), University of Tasmania (UTAS), Bureau of Meteorology (BoM), the Marine National Facility (MNF) and the Australian Antarctic Division (AAD). We also acknowledge the support of the CSIRO Moored Sensor Systems team."
 ncOut.setncattr("data_type", "OceanSITES time-series data")
 ncOut.setncattr("format_version", "1.3")
 ncOut.setncattr("network", "IMOS")
+# TODO: is this the right theme to use?
+ncOut.setncattr("theme", "Global Ocean Watch")
+ncOut.setncattr("summary", "Particle flux data from the Southern Ocean Time Series observatory in the Southern Ocean southwest of Tasmania.")
+# TODO: is this the correct terminology?
 ncOut.setncattr("institution_references", "http://www.oceansites.org, http://imos.org.au")
 ncOut.setncattr("id", outputName)
-ncOut.setncattr("area", "Pacific Ocean")
+ncOut.setncattr("sea_area", "Pacific Ocean")
 ncOut.setncattr("array", "SOTS")
 ncOut.setncattr("update_interval", "void")
-ncOut.setncattr("institution", "Commonwealth Scientific and Industrial Research Organisation (CSIRO)")
+ncOut.setncattr("creator_institution", "Commonwealth Scientific and Industrial Research Organisation (CSIRO)")
 ncOut.setncattr("source", "subsurface mooring")
 ncOut.setncattr("naming_authority", "OceanSITES")
 ncOut.setncattr("data_assembly_center", "IMOS")
+# TODO: this needs more information included, e.g. PI, etc.
 ncOut.setncattr("citation", "Ocean Sites. [year-of-data-download], [Title], [Data access URL], accessed [date- of-access]")
 ncOut.setncattr("Conventions", "CF-1.6 OceanSITES-1.3 NCADD-1.2.1")
 ncOut.setncattr("license", "Follows CLIVAR (Climate Variability and Predictability) standards,cf. http://www.clivar.org/data/data_policy.php Data available free of charge. User assumes all risk for use of  data. User must display citation in any publication or product using data. User must contact PI prior to any commercial use of data.")
-ncOut.setncattr("contributor_name", "CSIRO; IMOS; ACE-CRC; MNF")
+ncOut.setncattr("contributor_name", "CSIRO; IMOS; ACE-CRC; MNF; AAPP")
 ncOut.setncattr("processing_level", "data manually reviewed")
+# TODO: can we say "excellent" for the SAZ data since it is QCed?
 ncOut.setncattr("QC_indicator", "mixed")
-ncOut.setncattr("sensor_name", sensor + "-" + serialNumber)
+ncOut.setncattr("instrument", instrumentName + "-" + serialNumber)
 ncOut.setncattr("cdm_data_type", "Station")
+ncOut.setncattr("platform_deployment_date", deployment_start)
+ncOut.setncattr("platform_recovery_date", deployment_end)
 
-#
+# TODO: sort the attributes alphabetcially?
+
 # copyData
 #
 
